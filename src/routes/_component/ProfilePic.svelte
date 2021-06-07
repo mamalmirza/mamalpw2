f92d08788e5b72bd9bf1e014fade8e081c039146
mamal <mirzaei.mohammadreza1997@gmail.com>
2021-06-07 11:02:42 +0430

    feat: add netlify support

--- src/routes/_component/ProfilePic.svelte
+++ src/routes/_component/ProfilePic.svelte
@@ -3,7 +3,7 @@
         height: 400px;
         width: 400px;
         border-radius: 100%;
-        background: var(--orange-color);
+        background: var(--bg-color);
         border: 1px solid var(--text-color);
     }
 </style>
